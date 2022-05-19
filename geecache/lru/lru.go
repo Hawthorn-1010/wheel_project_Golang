f4ed17d036a163176e4a40e86b442102db59880a
Yan Cang <alailaikeai@163.com>
2022-05-19 10:42:32 +0800

    lru实现

---
+++ geecache/lru/lru.go
@@ -0,0 +1,89 @@
+package lru
+
+/**
+分布式缓存，k-v
+*/
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64 // 最大允许使用的内存
+	nBytes   int64 // 当前已使用的内存
+	ll       *list.List
+	// value 是双向链表中对应节点的指针
+	cache map[string]*list.Element
+	// 可选，在删除缓存时执行的回调函数
+	OnEvicted func(key string, value Value)
+}
+
+// 双向链表的节点
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value是自定义的类型，有属性，使用Len计算占用多少字节
+type Value interface {
+	Len() int
+}
+
+// 实例化cache
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// 删除
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		// ?
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		// 修改当前容量，减去key和value的长度
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			// ?
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	// ele : list.Element
+	if ele, ok := c.cache[key]; ok {
+		// 如果在链表里查找到，就说明原先就存在，只需放到链表的最头部，并覆盖原先key的value
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry) // 这个Value不是自己造的Value
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(len(kv.key)) + int64(kv.value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		// 超出容量，lru删除缓存
+		c.RemoveOldest()
+	}
+}
+
+// Len() 获取添加了多少条数据
+func (c *Cache) Len() int {
+	return c.ll.Len() // 此Len非彼Len
+}
---
+++ geecache/lru/lru_test.go
@@ -0,0 +1,74 @@
+package lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+
+func TestGet(t *testing.T) {
+	lru := New(int64(0), nil)
+	// 这个格式？
+	lru.Add("hello", String("world"))
+	// v.(String) 强制类型转换
+	if v, ok := lru.Get("hello"); !ok || v.(String) != "world" {
+		t.Fatalf("cache hit hello = world failed!")
+	}
+	if v, ok := lru.Get("hello"); ok {
+		t.Log("hello : " + v.(String))
+	}
+	if _, ok := lru.Get("hzy"); !ok {
+		t.Fatalf("cache miss hzy failed!")
+	}
+}
+
+func TestCache_RemoveOldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "value3"
+	// 初始化的大小只够存放两个k-v对
+	cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if v, ok := lru.Get(k2); !ok {
+		t.Fatalf("there is no key name " + k2)
+	} else {
+		t.Log("key2 : " + v.(String))
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	// 能放三组k-v
+	cap := 3 * len("k1"+"k2")
+	t.Log(cap)
+
+	// 在删除的时候才会调用回调
+	lru := New(int64(cap), callback)
+
+	lru.Add("k1", String("v1"))
+	lru.Add("k2", String("v2"))
+	lru.Add("k3", String("v3"))
+	lru.Add("k4", String("v4"))
+	lru.Add("k5", String("v5"))
+
+	t.Log(keys)
+
+	expect := []string{"k1", "k2"}
+
+	if !reflect.DeepEqual(expect, keys) {
+		//t.Log(keys)
+		t.Fatalf("Call onEvicted failed, expect keys equels to %s", expect)
+	}
+
+}
