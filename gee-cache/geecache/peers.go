17871ce82bdc11c1539d39bafbd6aa1119c319e8
Yan Cang <alailaikeai@163.com>
2022-05-20 21:43:56 +0800

    protobuf通信

--- gee-cache/geecache/peers.go
+++ gee-cache/geecache/peers.go
@@ -1,5 +1,7 @@
 package geecache
 
+import pb "gee-cache/geecache/geecachepb"
+
 // 根据传入的key选择相应的节点PeerGetter
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
@@ -7,5 +9,5 @@ type PeerPicker interface {
 
 // 从对应group中查找缓存值，对应于流程中的HTTP客户端
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
